819fc4cffedfb1b26523866943a4c5b4ef904fd6
Andre Costa <andrecondecosta@gmail.com>
2024-10-30 02:01:07 +0000

    fixed multi images

--- app/controllers/api/v1/horses_controller.rb
+++ app/controllers/api/v1/horses_controller.rb
@@ -1,66 +1,79 @@
+# app/controllers/api/v1/horses_controller.rb
 class Api::V1::HorsesController < ApplicationController
   before_action :set_horse, only: [:show, :update, :destroy]
 
-  # Lista todos os cavalos do usuário autenticado
   def index
-    @horses = current_user.horses.includes(image_attachment: :blob)
+    @horses = current_user.horses.includes(images_attachments: :blob)
     render json: @horses.map { |horse|
       horse.as_json.merge({
-        image_url: horse.image.attached? ? url_for(horse.image) : nil
+        images: horse.images.map { |image| url_for(image) }
       })
     }
   end
 
-  # Exibe um cavalo específico e suas mídias
   def show
     render json: @horse.as_json.merge({
-      image_url: @horse.image.attached? ? url_for(@horse.image) : nil
+      images: @horse.images.map { |image| url_for(image) }
     })
   end
 
-  # Cria um novo cavalo
   def create
     @horse = current_user.horses.build(horse_params)
-
     if @horse.save
-      if params[:horse][:image]
-        @horse.image.attach(params[:horse][:image])
+      if params[:horse][:images] && @horse.images.blank?
+        params[:horse][:images].each do |image|
+          @horse.images.attach(image)
+        end
       end
       render json: @horse.as_json.merge({
-        image_url: @horse.image.attached? ? url_for(@horse.image) : nil
+        images: @horse.images.map { |image| url_for(image) }
       }), status: :created
     else
       render json: @horse.errors, status: :unprocessable_entity
     end
   end
 
-  # Atualiza um cavalo existente
+  # app/controllers/api/v1/horses_controller.rb
   def update
     if @horse.update(horse_params)
+      # Remove imagens especificadas em deleted_images
+      if params[:deleted_images]
+        params[:deleted_images].each do |image_url|
+          image = @horse.images.find { |img| url_for(img) == image_url }
+          image.purge if image
+        end
+      end
+
+      # Anexa novas imagens, se existirem
+      if params[:horse][:images]
+        params[:horse][:images].each do |image|
+          @horse.images.attach(image)
+        end
+      end
+
       render json: @horse.as_json.merge({
-        image_url: @horse.image.attached? ? url_for(@horse.image) : nil
+        images: @horse.images.map { |image| url_for(image) }
       })
     else
       render json: @horse.errors, status: :unprocessable_entity
     end
   end
 
-  # Deleta um cavalo
+
+
   def destroy
-    @horse.image.purge if @horse.image.attached?
+    @horse.images.purge if @horse.images.attached?
     @horse.destroy
     head :no_content
   end
 
   private
 
-  # Encontra o cavalo baseado no ID
   def set_horse
     @horse = current_user.horses.find(params[:id])
   end
 
-  # Permite os parâmetros permitidos para criação e atualização de cavalo
   def horse_params
-    params.require(:horse).permit(:name, :age, :description, :image)
+    params.require(:horse).permit(:name, :age, :description, images: [])
   end
 end
