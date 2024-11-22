b0a8b3c6a114047ebe2324fbf05f503a68020b1b
Andre Costa <andrecondecosta@gmail.com>
2024-11-22 17:54:17 +0000

    production modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,5 +89,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exc
   config.hosts << "horsesinpocket-backend-2.onrender.com"
-  config.hosts << "horsesinpocket-frontend.onrender.com"
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
