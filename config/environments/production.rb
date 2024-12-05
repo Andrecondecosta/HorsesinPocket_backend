41fcbe6b375db3c8d4deec9ab8ef1d63580f19d8
Andre Costa <andrecondecosta@gmail.com>
2024-12-05 22:03:59 +0000

    modified:   app/controllers/api/v1/horses_controller.rb         modifi 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,18 +94,16 @@ Rails.application.configure do
   # config.host_authorization = { exc
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'smtp.gmail.com',
     port: 587,
-    domain: ENV["APP_HOST"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["EMAIL_USER"],
-    password: ENV["EMAIL_PASSWORD"],
-    open_timeout: 5,
-    read_timeout: 5
+    domain: 'horsesinpocket-backend-2.onrender.com',
+    user_name: ENV['EMAIL_USER'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
-
   config.action_mailer.default_url_options = { host: ENV['APP_HOST'], protocol: 'https' }
+
 
 
 
