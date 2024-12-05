b12ec45ca4c5263eadb88fdec9a27ff334fa1584
Andre Costa <andrecondecosta@gmail.com>
2024-12-05 22:50:06 +0000

    modified:   config/environments/production.rb 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
+    domain: 'horsesinpocket-backend-2.onrender.com',
     user_name: ENV['EMAIL_USER'],
     password: ENV['EMAIL_PASSWORD'],
     authentication: 'plain',
