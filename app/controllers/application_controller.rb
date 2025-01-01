a132faf9688d777eced8b7ac714ef7dadf6e8180
Andre Costa <andrecondecosta@gmail.com>
2025-01-01 23:44:11 +0000

    modified:   app/controllers/application_controller.rb         modified:   config/routes.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,4 +34,8 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, Rails.application.credentials.secret_key_base)
   end
+
+  def frontend_index
+    render file: Rails.root.join('public', 'index.html'), layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
         resources :photos, only: [:create, :destroy]
         resources :videos, only: [:create, :destroy]
         resources :xrays, only: [:create, :destroy]
-
       end
 
       resources :logs, only: [:index]
@@ -19,13 +18,9 @@ Rails.application.routes.draw do
       put '/update', to: 'registrations#update'
       get '/received', to: 'horses#received_horses'
       get 'home/index'
-
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Adicione esta rota para lidar com todas as rotas desconhecidas (catch-all)
+  get '*path', to: 'application#frontend_index', constraints: ->(req) { !req.xhr? && req.format.html? }
 end
