92da73dc61a1043b22c027e50c755d324044d67a
Andre Costa <andrecondecosta@gmail.com>
2025-01-03 10:55:00 +0000

    modified:   app/controllers/application_controller.rb 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,11 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    if decoded_token
-      user_id = decoded_token[0]['user_id']
-      @user = User.find_by(id: user_id)
+    @current_user ||= begin
+      if decoded_token
+        user_id = decoded_token[0]['user_id']
+        User.find_by(id: user_id)
+      end
     end
   end
 
