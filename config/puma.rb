c075141ede056422eeab16d11534408c57357de7
Andre Costa <andrecondecosta@gmail.com>
2025-01-04 17:04:24 +0000

    change puma2

--- config/puma.rb
+++ config/puma.rb
@@ -1,24 +1,23 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# Número de workers (threads paralelas por processo)
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-workers ENV.fetch("WEB_CONCURRENCY") { 2 } # Número de processos de workers
+# Configuração do número de threads mínimo e máximo
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-preload_app!
-
-rackup DefaultRackup
+# Porta padrão para a aplicação
 port ENV.fetch("PORT") { 3000 }
+
+# Ambiente de execução (development, production, etc.)
 environment ENV.fetch("RAILS_ENV") { "development" }
 
+# Pré-carrega a aplicação antes de forkar os workers
+preload_app!
+
+# Configuração do ActiveRecord em cada worker
 on_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
 
+# Permite reiniciar o Puma com o comando `rails restart`
 plugin :tmp_restart
