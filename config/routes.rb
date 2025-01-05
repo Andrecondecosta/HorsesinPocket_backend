5f8ee96d9fd66f3aa6b44754c7d2f0ae94b06a24
Andre Costa <andrecondecosta@gmail.com>
2025-01-05 23:09:16 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,13 +18,12 @@ Rails.application.routes.draw do
       put '/update', to: 'registrations#update'
       get '/received', to: 'horses#received_horses'
       get 'home/index'
+
+      # Rota para links partilhados no namespace
+      get 'horses/shared/:token', to: 'horses#shared', as: :shared_horse
     end
   end
-
-  # Rota para links partilhados, fora do namespace
-  get 'horses/shared/:token', to: 'horses#shared', as: :shared_horse
 end
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
