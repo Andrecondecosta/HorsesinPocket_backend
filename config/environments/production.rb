3e15cf930ed103bd723c88a4c6e96b69ca034637
Andre Costa <andrecondecosta@gmail.com>
2025-01-15 01:04:31 +0000

    url whatsapp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
   config.logger = Logger.new(STDOUT)
   config.logger.formatter = ::Logger::Formatter.new
 
-Rails.application.routes.default_url_options[:host] = 'horsesinpocket-frontend.onrender.com'
+Rails.application.routes.default_url_options[:host] = 'horsehub.info'
 Rails.application.routes.default_url_options[:protocol] = 'https'
 
 end
