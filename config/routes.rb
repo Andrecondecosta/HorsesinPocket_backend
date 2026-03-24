60a0725fe9f0482b0ca10f05112ed253e24c9b00
Andrecondecosta@gmail.com <Andrecondecosta@gmail.com>
2026-03-24 20:55:26 +0000

    modified:   config/routes.rb 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
       resources :horses do
         post :share_via_email, on: :member
         post :share_via_link, on: :member
-        get :received, on: :collection
         resources :ancestors, only: [:index, :create, :update, :destroy]
         resources :photos, only: [:create, :destroy]
         resources :videos, only: [:create, :destroy]
